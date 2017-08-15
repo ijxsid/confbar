96fb7c5f87d21b56d55577a18f7d28d28709fb98
FalloutX <ijxsid@gmail.com>
2017-08-15 14:10:12 +0530

    Add Conference Item Footer.

--- components/ConfList.js
+++ components/ConfList.js
@@ -15,7 +15,9 @@ const ConfContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 3fr;
   grid-template-rows: auto;
-  grid-template-areas: "sideimage info";
+  grid-template-areas:
+    "sideimage info"
+    "sideimage footer";
   margin: 20px 0;
   box-shadow: 0px 0px 12px 8px rgba(170, 170, 190, 0.15);
   background: white;
@@ -37,6 +39,14 @@ const FontIconStyled = styled.i`
   font-size: 0.74em;
   color: #818199;
 `
+const ConfFooter = styled.div`
+  grid-area: footer;
+  background-color: #FFFFD3;
+  align-self: end;
+  padding: 7px 20px;
+  font-weight: 600;
+  color: #E20049;
+`
 const Conference = ({ conf }) => (
   <ConfContainer>
     <ConfLogo src={conf.logo} />
@@ -55,6 +65,10 @@ const Conference = ({ conf }) => (
         <a href={conf.url}>{conf.url}</a>
       </div>
     </ConfInfo>
+    <ConfFooter>
+      <div>39 shares | 454 views | 8 Talks</div>
+
+    </ConfFooter>
   </ConfContainer>
 )
 
