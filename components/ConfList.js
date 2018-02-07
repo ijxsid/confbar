8fea5b1171cab73c16ea86a2eb986feb73f97f0d
FalloutX <ijxsid@gmail.com>
2018-02-07 23:32:24 +0530

    Make ConfList Responsive

--- components/ConfList.js
+++ components/ConfList.js
@@ -9,16 +9,30 @@ import StyledIcon from './styled/Icon'
 const Styled = {
   Container: styled.div` 
     width: 75%;
-    max-width: 850px;
-    min-width: 450px;
+    max-width: 960px;
+    min-width: 200px;
     margin: 0 auto;
-    padding: 15px 0;
+    padding: 1.5rem 0;
+
+    @media (max-width: 800px) {
+      width: 95%;
+      margin: 0 auto;
+    }
+    @media (min-width: 1720px) {
+      max-width: unset;
+      width: 75%;
+      margin: 0 auto;
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
+      grid-gap: 25px;
+    }
   `,
   Item: styled.div`
     display: grid;
-    grid-template-columns: 1fr 3fr;
+    grid-template-columns:  1fr 3fr;
     grid-template-rows: auto;
     grid-template-areas:
+      "sideimage title"
       "sideimage info"
       "sideimage footer";
     margin: 20px 0;
@@ -27,19 +41,108 @@ const Styled = {
     :hover {
       box-shadow: 0px 0px 20px 15px rgba(170, 170, 190, 0.1);
     }
+    
+    @media (min-width: 1720px) {
+      grid-template-columns: 100%;
+      grid-template-areas:
+      "title"
+      "sideimage"
+      "info"
+      "footer";
+      margin: 20px 25px;
+    }
+    @media (max-width: 960px) {
+      grid-template-columns: 33% 67%;
+    }
+
+    @media (max-width: 560px) {
+      grid-template-columns: 40% 60%;
+    }
+    @media (max-width: 480px) {
+      grid-template-columns: 100%;
+      grid-template-areas:
+      "title"
+      "sideimage"
+      "info"
+      "footer";
+      min-width: 280px;
+      width: 90%;
+      margin: 20px auto;
+    }
+  `,
+  Title: styled.div`
+    grid-area: title;
+    padding: 0.75rem 1.5rem;
+    font-weight: 700;
+    font-size: 1.4rem;
+    @media (max-width: 480px) {
+      a {
+        color: inherit;
+      }
+      text-align: center;
+      align-self: start;
+      font-size: 1.15rem;
+      color: #f0f0f0;
+      background-color: #3273dc;
+    }
+    @media (min-width: 1720px) {
+      a {
+        color: inherit;
+      }
+      a:hover {
+        color: #e2e2e2;
+      }
+      text-align: center;
+      font-size: 1.45rem;
+      align-self: start;
+      color: #fff;
+      background-color: #3273dc;
+    }
+
   `,
   Logo: styled.img`
     grid-area: sideimage;
     min-height: 200px;
+    margin: 0 auto;
+    align-self: center;
+    @media (max-width: 480px) {
+      height: 150px;
+      margin: 0 auto;
+      padding: 10px 20px;
+    }
+    @media (min-width: 1720px) {
+      height: 360px;
+      margin: 0 auto;
+      padding: 10px 20px;
+    }
+
   `,
   Info: styled.div`
     grid-area: info;
-    padding: 10px 20px;
+    padding: 0.75rem 1.5rem;
+    align-self: middle;
+
+    @media (min-width: 1720px) {
+      font-size: 1.3rem;
+      align-self: top;
+      padding: 1.5rem 1.5rem;
+    }
   `,
   Icon: styled.i`
     ${StyledIcon}
     font-size: 0.80em;
     color: #818199;
+    @media (max-width: 480px) {
+      color: #333;
+    }
+  `,
+  ConfLink: styled.a`
+    @media (max-width: 800px) {
+      word-break: break-all;
+    }
+    @media (min-width: 1720px) {
+      word-break: break-all;
+    }
   `,
   Footer: styled.div`
     grid-area: footer;
@@ -56,6 +159,9 @@ const Styled = {
     a:hover {
       color: #3273dc;
     }
+    @media (min-width: 1720px) {
+      font-size: 1.3rem;
+    }
   `,
   FooterIcon: styled.i`
     ${StyledIcon}
@@ -65,12 +171,12 @@ const Styled = {
 const Conference = ({ conf }) => (
   <Styled.Item>
     <Styled.Logo src={conf.logo} />
+    <Styled.Title>
+      <Link href={`/conference?id=${conf._id}`} as={`/conference/${conf._id}`}>
+        <a>{conf.name} - {conf.year}</a>
+      </Link>
+    </Styled.Title>
     <Styled.Info>
-      <div className="title is-4">
-        <Link href={`/conference?id=${conf._id}`} as={`/conference/${conf._id}`}>
-          <a>{conf.name} - {conf.year}</a>
-        </Link>
-      </div>
       <div><Styled.Icon className="icon-location" /> {conf.location}</div>
       <div>
         <Styled.Icon className="icon-calendar" />{` `}
@@ -78,7 +184,7 @@ const Conference = ({ conf }) => (
       </div>
       <div>
         <Styled.Icon className="icon-link" />{` `}
-        <a href={conf.url}>{conf.url}</a>
+        <Styled.ConfLink href={conf.url}>{conf.url}</Styled.ConfLink>
       </div>
     </Styled.Info>
     <Styled.Footer>
