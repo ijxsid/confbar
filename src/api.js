a39f6e43668a3d3e9d321ef33dc2fd0d75d440fd
FalloutX <ijxsid@gmail.com>
2018-02-11 21:50:49 +0530

    Add Speaker and Add Tag workers

--- src/api.js
+++ src/api.js
@@ -3,10 +3,12 @@ import fetch from 'isomorphic-unfetch'
 
 const TOKEN = config.confbar.token
 const apiBase = config.confbar.api.base
+const videosAPI = `${apiBase}/api/videos`
 
+const speakersAPI = `${apiBase}/api/speakers`
+const techAPI = `${apiBase}/api/technologies`
 
 export async function addVideo (videoData) {
-  const videosAPI = `${apiBase}/api/videos`
   const res = await fetch(videosAPI, {
     headers: {
       'Content-Type': 'application/json',
@@ -17,5 +19,47 @@ export async function addVideo (videoData) {
   })
   const data = await res.json()
 
+  return data
+}
+
+export async function editVideo (id, videoData) {
+  const url = `${apiBase}/api/videos/${id}`
+  const res = await fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `JWT ${TOKEN}`
+    },
+    method: 'PUT',
+    body: JSON.stringify(videoData)
+  })
+  const data = await res.json()
+
+  return data
+}
+
+
+export async function addSpeaker (speaker) {
+  const res = await fetch(speakersAPI, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `JWT ${TOKEN}`
+    },
+    method: 'POST',
+    body: JSON.stringify(speaker)
+  })
+  const data = await res.json()
+  return data
+}
+
+export async function addTech (techData) {
+  const res = await fetch(techAPI, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `JWT ${TOKEN}`
+    },
+    method: 'POST',
+    body: JSON.stringify(techData)
+  })
+  const data = await res.json()
   return data
 }
